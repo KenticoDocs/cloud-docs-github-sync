f8a98d9f3a0236c6dd5fbbfca54d686f8ae3555d
Vaclav Malinak <vaclavm@kentico.com>
2018-12-20 13:31:19 +0100

    test deploy

--- KenticoCloudDocsGithubService/Function1.cs
+++ KenticoCloudDocsGithubService/Function1.cs
@@ -20,6 +20,7 @@ namespace KenticoCloudDocsGithubService
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             string name = req.Query["name"];
+
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
