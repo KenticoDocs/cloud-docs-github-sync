2520d63b31d72cb61c0da37e69429d835bbef51e
Patrik Medveď <patrikm@kentico.com>
2019-01-15 10:27:40 +0100

    KA-462 Add injection of sensitive information

--- GithubService.Repository/CodeSampleFileRepository.cs
+++ GithubService.Repository/CodeSampleFileRepository.cs
@@ -1,31 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using GithubService.Models.CodeSamples;
+﻿using GithubService.Models.CodeSamples;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Table;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace GithubService.Repository
 {
     public class CodeSampleFileRepository : ICodeSampleFileRepository
     {
-        private static readonly string tableName = "FileCodeSamples";
-        private static readonly string storageAccountName = "kcddev";
-        private static readonly string storageAccountKey = "";
+        private readonly CloudTable _fileCodeSamplesTable;
 
-        private readonly CloudTable fileCodeSamplesTable;
-
-        public CodeSampleFileRepository()
+        public static async Task<CodeSampleFileRepository> CreateInstance(ICodeSampleFileRepositoryConfig config)
         {
-            var cloudStorageAccount = new CloudStorageAccount(new StorageCredentials(storageAccountName, storageAccountKey), true);
+            var instance = new CodeSampleFileRepository(config);
+            await instance._fileCodeSamplesTable.CreateIfNotExistsAsync();
+            return instance;
+        }
+
+        private CodeSampleFileRepository(ICodeSampleFileRepositoryConfig config)
+        {
+            var cloudStorageAccount = CloudStorageAccount.Parse(config.ConnectionString);
             var tableClient = cloudStorageAccount.CreateCloudTableClient();
 
-            fileCodeSamplesTable = tableClient.GetTableReference(tableName);
-
-            CreateTableIfNotExists().Wait();
+            _fileCodeSamplesTable = tableClient.GetTableReference(config.TableName);
         }
+
+        private CodeSampleFileRepository() { }
 
         public async Task<CodeSampleFile> ArchiveFileAsync(CodeSampleFile file)
         {
@@ -33,7 +36,8 @@ namespace GithubService.Repository
 
             return getDeserializedFileCodeSamples(await ExecuteTableOperation(TableOperation.Delete(fileDto)));
         }
-        public async Task<CodeSampleFile> GetFileAsync(String filePath)
+
+        public async Task<CodeSampleFile> GetFileAsync(string filePath)
             => getDeserializedFileCodeSamples(await ExecuteTableOperation(TableOperation.Retrieve<CodeSampleFileDto>(filePath, filePath)));
 
         public async Task<CodeSampleFile> UpdateFileAsync(CodeSampleFile fileToUpdate)
@@ -48,28 +52,29 @@ namespace GithubService.Repository
             var fileDto = getSerializedFileCodeSamples(newFile);
             var addedFile = await ExecuteTableOperation(TableOperation.InsertOrReplace(fileDto));
 
-            return getDeserializedFileCodeSamples(addedFile); 
+            return getDeserializedFileCodeSamples(addedFile);
         }
 
         private async Task<CodeSampleFileDto> ExecuteTableOperation(TableOperation operation)
         {
-            var executedOperation = await fileCodeSamplesTable.ExecuteAsync(operation);
+            var executedOperation = await _fileCodeSamplesTable.ExecuteAsync(operation);
 
             return executedOperation.Result as CodeSampleFileDto;
         }
 
-        private async Task CreateTableIfNotExists()
-        {
-            if (!await fileCodeSamplesTable.ExistsAsync())
-            {
-                await fileCodeSamplesTable.CreateAsync();
-            };
-        }
-
         private CodeSampleFileDto getSerializedFileCodeSamples(CodeSampleFile file)
-            => new CodeSampleFileDto() { FilePath = file.FilePath, CodeSamples = JsonConvert.SerializeObject(file.CodeSamples), ETag = "*" };
+            => new CodeSampleFileDto
+            {
+                FilePath = file.FilePath,
+                CodeSamples = JsonConvert.SerializeObject(file.CodeSamples),
+                ETag = "*"
+            };
 
         private CodeSampleFile getDeserializedFileCodeSamples(CodeSampleFileDto fileDto)
-            => new CodeSampleFile() { FilePath = fileDto.FilePath, CodeSamples = JsonConvert.DeserializeObject<List<CodeSample>>(fileDto.CodeSamples) };
+            => new CodeSampleFile
+            {
+                FilePath = fileDto.FilePath,
+                CodeSamples = JsonConvert.DeserializeObject<List<CodeSample>>(fileDto.CodeSamples)
+            };
     }
 }
