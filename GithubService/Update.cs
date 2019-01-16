e28238ca46857f1df35052f6dba72050b01a13f1
Juraj Bielik <jurajb@kentico.com>
2019-01-16 17:52:18 +0100

    KA-464 Implement adding new files

--- GithubService/Update.cs
+++ GithubService/Update.cs
@@ -1,7 +1,14 @@
+using System;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using GithubService.Models.Webhooks;
-using GithubService.Services.Parsers;
+using GithubService.Repository;
+using GithubService.Services;
+using GithubService.Services.Clients;
+using GithubService.Services.Converters;
+using GithubService.Services.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
@@ -14,20 +21,58 @@ namespace GithubService
     public static class Update
     {
         [FunctionName("kcd-github-service-update")]
-        public static IActionResult Run(
+        public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest request,
             ILogger logger)
         {
             logger.LogInformation("Update called.");
-            WebhookMessage webhookMessage;
 
+            var fileParser = new FileParser();
+
+            // Get all the files from GitHub
+            var githubClient = new GithubClient(
+                Environment.GetEnvironmentVariable("Github.RepositoryName"),
+                Environment.GetEnvironmentVariable("Github.RepositoryOwner"),
+                Environment.GetEnvironmentVariable("Github.AccessToken"));
+            var githubService = new Services.GithubService(githubClient, fileParser);
+
+            // Read Webhook message from GitHub
+            WebhookMessage webhookMessage;
             using (var streamReader = new StreamReader(request.Body, Encoding.UTF8))
             {
                 var requestBody = streamReader.ReadToEnd();
                 webhookMessage = JsonConvert.DeserializeObject<WebhookMessage>(requestBody);
             }
+
+            // Get paths to added/modified/deleted files
             var parser = new WebhookParser();
             var (addedFiles, modifiedFiles, removedFiles) = parser.ExtractFiles(webhookMessage);
+
+            var connectionString = Environment.GetEnvironmentVariable("Repository.ConnectionString");
+            var codeSampleFileRepository = await CodeSampleFileRepository.CreateInstance(connectionString);
+
+            var codeSamplesConverter = new CodeSamplesConverter();
+            var kenticoCloudClient = new KenticoCloudClient(
+                Environment.GetEnvironmentVariable("KenticoCloud.ProjectId"),
+                Environment.GetEnvironmentVariable("KenticoCloud.ContentManagementApiKey"),
+                Environment.GetEnvironmentVariable("KenticoCloud.InternalApiKey")
+            );
+
+            var kenticoCloudService = new KenticoCloudService(kenticoCloudClient, codeSamplesConverter);
+
+            if (addedFiles.ToList().Count > 0)
+            {
+                foreach (var filePath in addedFiles.ToList())
+                {
+                    // Persist each added file
+                    var codeSampleFile = await githubService.GetCodeSampleFileAsync(filePath);
+                    await codeSampleFileRepository.StoreAsync(codeSampleFile);
+
+                    // Create new KC content items
+                    var codenameCodeSample = codeSamplesConverter.ConvertToCodenameCodeSample(codeSampleFile);
+                    await kenticoCloudService.UpsertCodeBlockAsync(codenameCodeSample);
+                }
+            }
 
             // Parse the webhook message using IWebhookParser
             // Get the affected files using IGithubService.GetCodeSamplesFile
