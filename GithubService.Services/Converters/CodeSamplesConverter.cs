294401b6d641f92a90ccd67aefc109e2552a89ca
Juraj Bielik <jurajb@kentico.com>
2019-01-21 14:51:40 +0100

    KA-476 Assemble Initialize function

--- GithubService.Services/Converters/CodeSamplesConverter.cs
+++ GithubService.Services/Converters/CodeSamplesConverter.cs
@@ -1,16 +1,44 @@
 ﻿using GithubService.Models.CodeSamples;
 using GithubService.Models.KenticoCloud;
 using GithubService.Services.Interfaces;
-using System;
 using System.Collections.Generic;
 
 namespace GithubService.Services.Converters
 {
     public class CodeSamplesConverter : ICodeSamplesConverter
     {
-        public IEnumerable<CodenameCodeSamples> ConvertToCodenameCodeSamples(IEnumerable<CodeSampleFile> codeSampleFiles)
+        public IEnumerable<CodenameCodeSamples> ConvertToCodenameCodeSamples(
+            IEnumerable<CodeSampleFile> codeSampleFiles)
         {
-            throw new NotImplementedException();
+            var codenameCodeSamples = new Dictionary<string, CodenameCodeSamples>();
+
+            foreach (var codeSampleFile in codeSampleFiles)
+            {
+                foreach (var codeSample in codeSampleFile.CodeSamples)
+                {
+                    var codename = codeSample.Codename;
+                    var language = codeSample.Language;
+                    var content = codeSample.Content;
+
+                    if (codenameCodeSamples.ContainsKey(codename))
+                    {
+                        codenameCodeSamples[codename].CodeSamples.Add(language, content);
+                    }
+                    else
+                    {
+                        codenameCodeSamples.Add(codename, new CodenameCodeSamples
+                        {
+                            Codename = codename,
+                            CodeSamples = new Dictionary<CodeLanguage, string>
+                            {
+                                {language, content}
+                            }
+                        });
+                    }
+                }
+            }
+
+            return codenameCodeSamples.Values;
         }
 
         public CodeBlock ConvertToCodeBlock(CodenameCodeSamples codenameCodeSample)
--- GithubService/Initialize.cs
+++ GithubService/Initialize.cs
@@ -8,6 +8,8 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using GithubService.Repository;
+using GithubService.Services.Services;
 
 namespace GithubService
 {
@@ -20,20 +22,29 @@ namespace GithubService
         {
             logger.LogInformation("Initialize called.");
 
-            // Get all the files from GitHub - use IGithubService.GetCodeSamplesFiles
+            var fileParser = new FileParser();
+
+            // Get all the files from GitHub
             var githubClient = new GithubClient(
                 Environment.GetEnvironmentVariable("Github.RepositoryName"),
                 Environment.GetEnvironmentVariable("Github.RepositoryOwner"),
                 Environment.GetEnvironmentVariable("Github.AccessToken"));
-            var githubService = new Services.GithubService(githubClient, null);
+            var githubService = new Services.GithubService(githubClient, fileParser);
             var codeSampleFiles = await githubService.GetCodeSampleFilesAsync();
-            // Persist all code sample files using ICodeSampleFileRepository
 
-            // Convert those files using ICodeSamplesConverter.ConvertToCodenameCodeSamples
+            // Persist all code sample files
+            var connectionString = Environment.GetEnvironmentVariable("Repository.ConnectionString");
+            var codeSampleFileRepository = await CodeSampleFileRepository.CreateInstance(connectionString);
+
+            foreach (var codeSampleFile in codeSampleFiles)
+            {
+                await codeSampleFileRepository.StoreAsync(codeSampleFile);
+            }
+
             var codeSamplesConverter = new CodeSamplesConverter();
-            var samplesByCodename = codeSamplesConverter.ConvertToCodenameCodeSamples(null);
+            var samplesByCodename = codeSamplesConverter.ConvertToCodenameCodeSamples(codeSampleFiles);
 
-            // Create/update appropriate KC items using IKenticoCloudService
+            // Create/update appropriate KC items
             var kenticoCloudClient = new KenticoCloudClient(
                 Environment.GetEnvironmentVariable("KenticoCloud.ProjectId"),
                 Environment.GetEnvironmentVariable("KenticoCloud.ContentManagementApiKey"),
