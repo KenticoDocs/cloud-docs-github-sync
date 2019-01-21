5f45ad8d39fceca3a64bbdcf6ef8f6fdeed67f75
Juraj Bielik <jurajb@kentico.com>
2019-01-21 07:59:54 +0100

    KA-463 Add unit tests for commit optimization scenarios

--- GithubService.Services.Tests/Services/WebhookParserTests.cs
+++ GithubService.Services.Tests/Services/WebhookParserTests.cs
@@ -18,11 +18,12 @@ namespace GithubService.Services.Tests.Services
         private readonly IWebhookParser _webhookParser = new WebhookParser();
 
         [Test]
-        public void ParseFiles_WebhookMessageSingleCommit_ReturnsTupleWithAddedModifiedDeletedFiles()
+        public void ExtractFiles_WebhookMessageSingleCommit_ReturnsCorrectTupleWithAddedModifiedDeletedFiles()
         {
             var expectedAddedFiles = new[] {TestFileA};
             var expectedModifiedFiles = new string[] { };
             var expectedRemovedFiles = new[] {TestFileC, TestFileD};
+            var expectedOutput = (expectedAddedFiles, expectedModifiedFiles, expectedRemovedFiles);
             var webhookMessage = new WebhookMessage
             {
                 Commits = new List<Commit>
@@ -36,19 +37,18 @@ namespace GithubService.Services.Tests.Services
                 }
             };
 
-            var (addedFiles, modifiedFiles, removedFiles) = _webhookParser.ParseFiles(webhookMessage);
+            var actualOutput = _webhookParser.ExtractFiles(webhookMessage);
 
-            CollectionAssert.AreEquivalent(addedFiles, expectedAddedFiles);
-            CollectionAssert.AreEquivalent(modifiedFiles, expectedModifiedFiles);
-            CollectionAssert.AreEquivalent(removedFiles, expectedRemovedFiles);
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
         }
 
         [Test]
-        public void ParseFiles_WebhookMessageMultipleCommits_ReturnsTupleWithAddedModifiedDeletedFiles()
+        public void ExtractFiles_WebhookMessageMultipleCommits_ReturnsCorrectTupleWithAddedModifiedDeletedFiles()
         {
             var expectedAddedFiles = new[] {TestFileB, TestFileC};
             var expectedModifiedFiles = new[] {TestFileD};
             var expectedRemovedFiles = new string[] { };
+            var expectedOutput = (expectedAddedFiles, expectedModifiedFiles, expectedRemovedFiles);
             var webhookMessage = new WebhookMessage
             {
                 Commits = new List<Commit>
@@ -80,11 +80,172 @@ namespace GithubService.Services.Tests.Services
                 }
             };
 
-            var (addedFiles, modifiedFiles, removedFiles) = _webhookParser.ParseFiles(webhookMessage);
+            var actualOutput = _webhookParser.ExtractFiles(webhookMessage);
 
-            CollectionAssert.AreEquivalent(addedFiles, expectedAddedFiles);
-            CollectionAssert.AreEquivalent(modifiedFiles, expectedModifiedFiles);
-            CollectionAssert.AreEquivalent(removedFiles, expectedRemovedFiles);
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        public void ExtractFiles_FileAddedAndThenModified_ReturnsAsAddedFile()
+        {
+            var expectedAddedFiles = new [] {TestFileA};
+            var expectedOutput = (expectedAddedFiles, new string[] { }, new string[] { });
+            var webhookMessage = new WebhookMessage
+            {
+                Commits = new List<Commit>
+                {
+                    new Commit
+                    {
+                        Added = new List<string> {TestFileA},
+                        Modified = new List<string>(),
+                        Removed = new List<string>()
+                    },
+                    new Commit
+                    {
+                        Added = new List<string>(),
+                        Modified = new List<string> {TestFileA},
+                        Removed = new List<string>()
+                    }
+                }
+            };
+
+            var actualOutput = _webhookParser.ExtractFiles(webhookMessage);
+
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        public void ExtractFiles_FileAddedAndModifiedAndThenRemoved_ReturnsNoFile()
+        {
+            var expectedOutput = (new string[] { }, new string[] { }, new string[] { });
+            var webhookMessage = new WebhookMessage
+            {
+                Commits = new List<Commit>
+                {
+                    new Commit
+                    {
+                        Added = new List<string> {TestFileA},
+                        Modified = new List<string>(),
+                        Removed = new List<string>()
+                    },
+                    new Commit
+                    {
+                        Added = new List<string>(),
+                        Modified = new List<string> {TestFileA},
+                        Removed = new List<string>()
+                    },
+                    new Commit
+                    {
+                        Added = new List<string>(),
+                        Modified = new List<string>(),
+                        Removed = new List<string> {TestFileA}
+                    }
+                }
+            };
+
+            var actualOutput = _webhookParser.ExtractFiles(webhookMessage);
+
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        public void ExtractFiles_FileAddedAndThenRemoved_ReturnsNoFile()
+        {
+            var expectedOutput = (new string[] { }, new string[] { }, new string[] { });
+            var webhookMessage = new WebhookMessage
+            {
+                Commits = new List<Commit>
+                {
+                    new Commit
+                    {
+                        Added = new List<string> {TestFileA},
+                        Modified = new List<string>(),
+                        Removed = new List<string>()
+                    },
+                    new Commit
+                    {
+                        Added = new List<string>(),
+                        Modified = new List<string>(),
+                        Removed = new List<string> {TestFileA}
+                    }
+                }
+            };
+
+            var actualOutput = _webhookParser.ExtractFiles(webhookMessage);
+
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        public void ExtractFiles_FileRemovedAndThenAdded_ReturnsAsAddedFile()
+        {
+            var expectedAddedFiles = new [] {TestFileA};
+            var expectedOutput = (expectedAddedFiles, new string[] { }, new string[] { });
+            var webhookMessage = new WebhookMessage
+            {
+                Commits = new List<Commit>
+                {
+                    new Commit
+                    {
+                        Added = new List<string>(),
+                        Modified = new List<string>(),
+                        Removed = new List<string> {TestFileA}
+                    },
+                    new Commit
+                    {
+                        Added = new List<string> {TestFileA},
+                        Modified = new List<string>(),
+                        Removed = new List<string>()
+                    }
+                }
+            };
+
+            var actualOutput = _webhookParser.ExtractFiles(webhookMessage);
+
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        public void ExtractFiles_FileModifiedAndThenRemoved_ReturnsAsRemovedFile()
+        {
+            var expectedRemovedFiles = new [] {TestFileA};
+            var expectedOutput = (new string[] { }, new string[] { }, expectedRemovedFiles);
+            var webhookMessage = new WebhookMessage
+            {
+                Commits = new List<Commit>
+                {
+                    new Commit
+                    {
+                        Added = new List<string>(),
+                        Modified = new List<string> {TestFileA},
+                        Removed = new List<string>()
+                    },
+                    new Commit
+                    {
+                        Added = new List<string>(),
+                        Modified = new List<string>(),
+                        Removed = new List<string> {TestFileA}
+                    }
+                }
+            };
+
+            var actualOutput = _webhookParser.ExtractFiles(webhookMessage);
+
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        public void ExtractFiles_WebhookMessageNoCommit_ReturnsEmptyTuple()
+        {
+            var expectedOutput = (new string[] { }, new string[] { }, new string[] { });
+            var webhookMessage = new WebhookMessage
+            {
+                Commits = new List<Commit>()
+            };
+
+            var actualOutput = _webhookParser.ExtractFiles(webhookMessage);
+
+            Assert.That(actualOutput, Is.EqualTo(expectedOutput));
         }
     }
 }
