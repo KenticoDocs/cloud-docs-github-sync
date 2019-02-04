ef3f646486415025d224592b17479870556e09cc
Juraj Bielik <jurajb@kentico.com>
2019-02-04 12:13:16 +0100

    KA-480 Delete unused extension method

--- GithubService.Models/CodeFragmentLanguage.cs
+++ GithubService.Models/CodeFragmentLanguage.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace GithubService.Models
+﻿namespace GithubService.Models
 {
     public enum CodeFragmentLanguage
     {
@@ -17,33 +15,6 @@ namespace GithubService.Models
 
     public static class EnumExtensions
     {
-        public static string GetCodename(this CodeFragmentLanguage language)
-        {
-            switch (language)
-            {
-                case CodeFragmentLanguage.CUrl:
-                    return "curl";
-                case CodeFragmentLanguage.CSharp:
-                    return "c_";
-                case CodeFragmentLanguage.JavaScript:
-                    return "js";
-                case CodeFragmentLanguage.TypeScript:
-                    return "ts";
-                case CodeFragmentLanguage.Java:
-                    return "java";
-                case CodeFragmentLanguage.JavaRx:
-                    return "javarx";
-                case CodeFragmentLanguage.PHP:
-                    return "php";
-                case CodeFragmentLanguage.Swift:
-                    return "swift";
-                case CodeFragmentLanguage.Ruby:
-                    return "ruby";
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(language), language, "Such language does not exist");
-            }
-        }
-
         public static string GetCommentPrefix(this CodeFragmentLanguage language)
         {
             if (language == CodeFragmentLanguage.Ruby || language == CodeFragmentLanguage.CUrl)
