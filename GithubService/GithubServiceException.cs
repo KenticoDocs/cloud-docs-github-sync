909a84a9073418b4f55817ce18f1cfc9bf605641
Juraj Bielik <jurajb@kentico.com>
2019-04-29 17:09:53 +0200

    KA-538 Log exception messages on Azure

---
+++ GithubService/GithubServiceException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace GithubService
+{
+    [Serializable]
+    public sealed class GithubServiceException : Exception
+    {
+        public GithubServiceException(string message)
+            : base(message)
+        {
+        }
+
+        private GithubServiceException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
