73a0d33e5f0ba1d62f58766708f97bf03e2cbcf4
Miroslav Kubus <MiroslavK@kentico.com>
2019-06-26 09:41:23 +0200

    KA-550 Extract FileProcessor and test it

---
+++ GithubService.Services/Interfaces/IFileProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using GithubService.Models;
+using Microsoft.Extensions.Logging;
+
+namespace GithubService.Services.Interfaces
+{
+    public interface IFileProcessor
+    {
+        Task<IEnumerable<CodeFragment>> ProcessAddedFiles(ICollection<string> addedFiles);
+
+        Task<(IEnumerable<CodeFragment>, IEnumerable<CodeFragment>, IEnumerable<CodeFragment>)> ProcessModifiedFiles(
+            ICollection<string> modifiedFiles,
+            ILogger logger
+        );
+
+        Task<IEnumerable<CodeFragment>> ProcessRemovedFiles(ICollection<string> removedFiles);
+    }
+}
